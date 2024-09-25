7c6727e4605112459bb13dc4cc17eb9893eee223
Kate <166659076+kate00001@users.noreply.github.com>
2024-09-25 15:18:35 +0300

    Add files via upload

---
+++ module_5_4.py
@@ -0,0 +1,29 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        history = super(House, cls).__new__(cls)
+        cls.houses_history.append(args[0])
+        return history
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+del h2
+del h3
+print(House.houses_history)
