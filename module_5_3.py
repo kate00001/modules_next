c47c985eb7299ec1eb0760064fd96687d5bb576d
Kate <166659076+kate00001@users.noreply.github.com>
2024-09-25 16:34:48 +0300

    Update module_5_3.py
    Исправила ДЗ

--- module_5_3.py
+++ module_5_3.py
@@ -1,10 +1,7 @@
 class House:
     def __init__(self, name, number_of_floors):
-        if isinstance(number_of_floors, int):
-            self.name = name
-            self.number_of_floors = number_of_floors
-        else:
-            print("Число этажей должно быть целым числом")
+        self.name = name
+        self.number_of_floors = number_of_floors
 
     def __len__(self):
         return self.number_of_floors
@@ -13,51 +10,62 @@ class House:
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        return self.__eq__(other) or self.__lt__
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        return not self.__lt__(other)
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
 
     def __add__(self, value):
-        value = 5
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
-    def __radd__(self, value):
-        return self.__add__(value)
+    def __radd__(self, other):
+        return self.__add__(other)
 
-    def __iadd__(self, value):
-        return self.__add__(value)
+    def __iadd__(self, other: int):
+        return self.__add__(other)
 
 
-house1 = House('ЖК Эльбрус', 20)
-house2 = House('ЖК Акация', 25)
+h1 = House('ЖК Эльбрус', 20)
+h2 = House('ЖК Акация', 30)
 
-print(house1)
-print(house2)
+print(h1)
+print(h2)
 
-print(len(house1))
-print(len(house2))
+print(h1 == h2)    # __eq__
 
-print(house1 == house2)
-print(house1 < house2)
-print(house1 <= house2)
-print(house1 > house2)
-print(house1 >= house2)
-print(house1 != house2)
+h1 = h1 + 10       # __add__
+print(h1)
+print(h1 == h2)
 
-print(house1 + 5)
-print(5 + house1)
-print(house1)
+h1 += 10          # __iadd__
+print(h1)
+
+h2 = 10 + h2      # __radd__
+print(h2)
+
+print(h1 > h2)    # __gt__
+print(h1 >= h2)   # __ge__
+print(h1 < h2)    # __lt__
+print(h1 <= h2)   # __le__
+print(h1 != h2)   # __ne__
