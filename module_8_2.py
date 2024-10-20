bdbf2cbfa183cb80354999e2bb21b01e707a8642
Kate <166659076+kate00001@users.noreply.github.com>
2024-10-20 21:39:45 +0300

    Add files via upload

---
+++ module_8_2.py
@@ -0,0 +1,47 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+
+    for item in numbers:
+        try:
+            result += item
+        except TypeError:
+            print(f'Некорректный тип данных для подсчёта суммы - {item}')
+            incorrect_data += 1
+
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
+    try:
+        if not isinstance(numbers, (list, tuple, set)):
+            raise TypeError
+        total_sum, incorrect_data = personal_sum(numbers)
+        if len(numbers) - incorrect_data == 0:
+            return 0
+        return total_sum / (len(numbers) - incorrect_data)
+
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+        return None
+    except ZeroDivisionError:
+        return 0
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
+
+"""
+Вывод:
+
+В numbers записан некорректный тип данных
+Результат 1: None
+Некорректный тип данных для подсчёта суммы - Строка
+Некорректный тип данных для подсчёта суммы - Ещё Строка
+Результат 2: 2.0
+В numbers записан некорректный тип данных
+Результат 3: None
+Результат 4: 26.5
+"""
